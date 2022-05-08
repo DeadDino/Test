5e416af8c53aa01ceec6f3686d114d863d260407
DeadDino <105149032+DeadDino@users.noreply.github.com>
2022-05-08 23:38:45 +0300

    Update main.sql

--- main.sql
+++ main.sql
@@ -3,7 +3,7 @@ SELECT pilot_id, name, age, rank, education_level
 FROM Пилоты
 INNER JOIN Рейсы
     ON Рейсы.second_pilot_id = Пилоты.pilot_id
-WHERE count(Рейсы.destination) = 3 AND Рейсы.destination = 'Шереметьево'
+WHERE count(Рейсы.destination) = 3 AND Рейсы.destination = 'Шереметьево';
 
 -- Задача 2
 SELECT pilot_id, name, age, rank, education_level
@@ -20,10 +20,10 @@ FROM Пилоты
 INNER JOIN Рейсы
     ON Рейсы.first_pilot_id = Пилоты.pilot_id
 INNER JOIN Самолеты
-    ON Пилоты.plane_id = Самолеты.plane_id 
-GROUP BY name 
+    ON Пилоты.plane_id = Самолеты.plane_id  
 WHERE Самолеты.cargo_flg = 0 AND YEAR(Рейсы.flight_dt)=2022
 ORDER BY COUNT(Рейсы.pilot_id)
+LIMIT 10;
 
 
     
